5928cad7dc9d6af49e69334f3e08ae32dafc632a
thirofoo <83126064+thirofoo@users.noreply.github.com>
2024-05-18 14:08:35 +0900

    chore: tool 系統準備

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::cmp::Reverse;
+use std::collections::BinaryHeap;
+
 fn main() {
-    println!("Hello, world!");
+    let mut heap = BinaryHeap::new();
+    heap.push(Reverse(5));
+    heap.push(Reverse(1));
+    heap.push(Reverse(10));
+    
+    while let Some(Reverse(value)) = heap.pop() {
+        println!("{}", value); // 出力: 1, 5, 10
+    }
 }
